82bcc04a9b80d47f82c270af4f78de451164619c
Andy Thompson <athompson@inviqa.com>
2016-07-27 10:08:56 +0100

    Allow for SAN grouping of certificates
    Also switching to use a more standard site attribute location

--- recipes/cdh.rb
+++ recipes/cdh.rb
@@ -1,17 +1,17 @@
-split_domains = {}
+group_domains = {}
 %w{ apache nginx }.each do |server|
   next unless node[server] && node[server]['sites']
 
   node[server]['sites'].each do |name, site|
-    split_name = site['certbot'] && site['certbot']['split'] ? name : 'shared'
-    split_domains[split_name] ||= []
-    split_domains[split_name] << site['server_name']
-    split_domains[split_name] += site['server_aliases'] if site['server_aliases']
+    group_name = site['ssl'] && (site['ssl']['san_group'] || (site['ssl']['use_sni'] ? name : 'shared'))
+    group_domains[group_name] ||= []
+    group_domains[group_name] << site['server_name']
+    group_domains[group_name] += site['server_aliases'] if site['server_aliases']
   end
 end
 
-split_domains.each do |split_name, domains|
-  certbot_certonly split_name do
+group_domains.each do |group_name, domains|
+  certbot_certonly group_name do
     webroot true
     webroot_path node['certbot']['sandbox']['webroot_path']
     email node['certbot']['cert-owner']['email']
--- spec/recipes/cdh_spec.rb
+++ spec/recipes/cdh_spec.rb
@@ -25,32 +25,44 @@ describe 'certbot::cdh' do
       ChefSpec::SoloRunner.new do |node|
         node.set['nginx']['sites']['mysite1'] = {
           server_name: 'mysite1.dev',
-          certbot: {
-            split: true
-          }
-        }
-        node.set['nginx']['sites']['mysite3'] = {
-          server_name: 'mysite3.dev',
-          certbot: {
-            split: true
+          ssl: {
+            use_sni: true
           }
         }
         node.set['nginx']['sites']['mysite2'] = {
           server_name: 'mysite2.dev',
           server_aliases: ['js.mysite2.dev', 'css.mysite2.dev'],
-          certbot: {
-            split: true
+          ssl: {
+            use_sni: true
+          }
+        }
+        node.set['nginx']['sites']['mysite3'] = {
+          server_name: 'mysite3.dev',
+          ssl: {
+            san_group: 'mysite34'
+          }
+        }
+        node.set['nginx']['sites']['mysite4'] = {
+          server_name: 'mysite4.dev',
+          ssl: {
+            san_group: 'mysite34'
           }
         }
         node.set['certbot']['cert-owner']['email'] = 'root@localhost'
       end.converge(described_recipe)
     end
 
-    it "will create a separate certificate per site" do
+    it "will create a separate certificate per site when use_sni is on" do
       expect(chef_run).to run_execute('certbot certonly --webroot --webroot-path /var/www/certbot --email root@localhost --domains mysite1.dev --expand --agree-tos --non-interactive').with({
         user: 'certbot'
       })
       expect(chef_run).to run_execute('certbot certonly --webroot --webroot-path /var/www/certbot --email root@localhost --domains mysite2.dev,js.mysite2.dev,css.mysite2.dev --expand --agree-tos --non-interactive').with({
+        user: 'certbot'
+      })
+    end
+
+    it "will create a group of certificates per san_group" do
+      expect(chef_run).to run_execute('certbot certonly --webroot --webroot-path /var/www/certbot --email root@localhost --domains mysite3.dev,mysite4.dev --expand --agree-tos --non-interactive').with({
         user: 'certbot'
       })
     end
