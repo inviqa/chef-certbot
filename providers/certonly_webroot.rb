1f9acea197a98f3a4a3d88b96c7ebc843df9afbf
Khaled Blah <kblah@avarteq.de>
2017-03-06 12:33:13 +0100

    make sure flags are rem. if their value is false

--- providers/certonly_webroot.rb
+++ providers/certonly_webroot.rb
@@ -5,8 +5,8 @@ action :create do
     'config-dir' => node['certbot']['config_dir'],
     'work-dir' => node['certbot']['work_dir'],
     'logs-dir' => node['certbot']['logs_dir'],
-    'server' => node['certbot']['server'],
 
+    'authenticator' => 'webroot',
     'webroot' => true,
     'webroot-path' => new_resource.webroot_path,
     'email' => new_resource.email,
@@ -18,14 +18,29 @@ action :create do
     'non-interactive' => true
   }
 
+  if node['certbot']['server'] && !new_resource.staging
+    options['server'] = node['certbot']['server']
+  end
+
   unless options['agree-tos']
     raise 'You need to agree to the Terms of Service by setting agree_tos true'
   end
 
+  # certbot fails if certain parameters are specified with a false value, so
+  # here we make sure to remove those special cases
+  flags = %w(non-interactive noninteractive force-interactive dry-run
+             keep-until-expiring keep reinstall expand force-renewal
+             renew-by-default allow-subset-of-names duplicate os-packages-only
+             no-self-upgrade must-staple hsts test-cert staging debug
+             no-verify-ssl break-my-certs)
+  options.each { |key, value| options.delete(key) if !value and \
+    flags.include?(key) }
+
   chef_gem 'inifile'
 
   require 'inifile'
-  iniobject = IniFile.new(options)
+  iniobject = IniFile.new(:default => 'global')
+  iniobject['global'] = options
 
   configfile = "#{node['certbot']['work_dir']}/#{new_resource.name}.ini"
   file configfile do
