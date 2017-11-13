b28cc95edebbf6efd89e54cb286db584e18dfd8f
Khaled Blah <kblah@avarteq.de>
2017-11-13 12:01:29 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'athompson@inviqa.com'
 license 'apache2'
 description 'Installs/Configures certbot'
 long_description 'Installs/Configures certbot'
-version '0.2.1'
+version '0.2.2'
 
 issues_url 'https://github.com/inviqa/chef-certbot/issues'
 source_url 'https://github.com/inviqa/chef-certbot'
