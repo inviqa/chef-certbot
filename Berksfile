92bedc61c3218ccabc77d6f51f1d2abab7491723
Khaled Blah <kblah@avarteq.de>
2018-02-14 11:18:57 +0100

    Added integration tests (serverspec)
      * Bumped version to 0.3.0
      * Nginx dependency without version qualifier
      * Integration tests for serverspec added with test-helper cookbook
      * Added new suite "sandbox" suite to .kitchen.yml

--- Berksfile
+++ Berksfile
@@ -2,6 +2,7 @@ source 'https://supermarket.chef.io'
 
 group :integration do
   cookbook 'fake', :path => 'spec/cookbooks/fake'
+  cookbook 'test-helper'
 end
 
 metadata
--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'serverspec'
 
 if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
@@ -6,3 +7,7 @@ else
   set :backend, :cmd
   set :os, family: 'windows'
 end
+
+NODE_JSON_PATH = '/tmp/test-helper/node.json'
+raise 'Could not find node.json, is test-helper::default in the runlist?' unless ::File.exists?(NODE_JSON_PATH)
+$node = ::JSON.parse(File.read(NODE_JSON_PATH))
